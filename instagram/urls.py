c41faf6c3c80fbdbbbf4a8e889f7148f17c1a368
sharon0812 <anyangosharon@gmail.com>
2021-01-16 16:21:12 +0300

    import user

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,6 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
