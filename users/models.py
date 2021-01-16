c25c4053da1ce7e1ed919f7b886847b511319856
sharon0812 <anyangosharon@gmail.com>
2021-01-16 16:35:14 +0300

    modify users models.py

--- users/models.py
+++ users/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
