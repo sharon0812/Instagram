069cf0bf427fd7c8eb3f5f6bfc50e14343a2e1f3
sharon0812 <anyangosharon@gmail.com>
2021-01-16 16:34:08 +0300

    add user folder

--- instagram/settings.py
+++ instagram/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import os.path
+from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'insta',
+    'users',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
---
+++ users/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    following = models.ManyToManyField(User, related_name='following', blank=True, symmetrical=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
+
+    def profiles_posts(self):
+        return self.post_set.all()
+    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+        
+    def __str__(self):
+        return f'{self.user} Profile'
+
+    class Meta:
+        ordering = ('-created',)
+
+def save(self, **kwargs):
+    #save method for images
+    super().save()
+    
+    img = Image.open(self.image.path)
+    # method for resizing images
+    if img.height > 300 or img.width > 300:
+        output_size = (300, 300)
+        img.thumbnail(output_size)
+        img.save(self.image.path)
+        
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    
