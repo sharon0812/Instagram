05da1101d1f7f0ec05d94689716734253535be25
sharon0812 <anyangosharon@gmail.com>
2021-01-18 13:20:45 +0300

    modify templates

--- instagram/settings.py
+++ instagram/settings.py
@@ -84,8 +84,7 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',       
         'NAME': 'instagram',
         'USER': 'moringa',
         'PASSWORD':'Access',
